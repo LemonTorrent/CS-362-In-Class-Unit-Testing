4dc2b3e84d7315905440abbaa7a1b0ff05e5af01
Nicole <nyarbrough14@gmail.com>
2021-04-29 17:01:59 -0700

    Updated the unit tests in test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -3,19 +3,25 @@ import calc
 
 class testCaseAdd(unittest.TestCase):
     def test_add(self):
-        # passed conditions:
         self.assertEqual(calc.add(2, 1), 3)
-        self.assertEqual(calc.sub(4, 2), 2)
-        self.assertEqual(calc.mul(3, 9), 27)
-        self.assertEqual(calc.div(9, 3), 3)
-        self.assertEqual(calc.errorHandle(3, 2), True)
-
-        # failed conditions:
-        self.assertEqual(calc.errorHandle(3, "two"), True)
         self.assertEqual(calc.add(2, 9), 7)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(4, 2), 2)
         self.assertEqual(calc.sub(0, 4), 1)
+
+    def test_mul(self):
         self.assertEqual(calc.mul(3, 9), 27)
+        self.assertEqual(calc.mul(3, 9), 27)
+
+    def test_div(self):
+        self.assertEqual(calc.div(9, 3), 3)
         self.assertEqual(calc.div(10, 3), -1)
+
+    def test_input(self):
+        self.assertEqual(calc.errorHandle(3, 2), True)
+        self.assertEqual(calc.errorHandle(3, "two"), True)
+
 
 if __name__ == '__main__':
     unittest.main()
